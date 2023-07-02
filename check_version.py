97399503132014844c2f6fd822f78a25d077a4d5
root <root@casa>
2023-07-03 00:11:01 +0200

    push2

--- check_version.py
+++ check_version.py
@@ -2,13 +2,26 @@
 
 import requests
 
-version = 'v0.1.0'
+version = 'v0.0.1'
 
 def check_version():
+    # Consulto los realease
     response = requests.get(f"https://api.github.com/repos/sergio1986bcn/awshandler/releases")
     releases = response.json()
     
-    latest_release = releases[0]  # La API de GitHub devuelve las releases en orden, con la más reciente primero
-    print(latest_release['tag_name'])
+    # Último release
+    latest_release = releases[0]['tag_name']
+
+    if latest_release:
+        # Quito la "v" de las versiones y convierto el número en entero
+        version_number = list(map(int, version.split('v')[-1].split('.')))
+        latest_release_number = list(map(int, latest_release.split('v')[-1].split('.')))
+
+        if version_number < latest_release_number:
+            print("Hay una nueva versión disponible.")
+            answer = input("¿Quieres instalarla? (s/n)")
+            if answer.lower() == 's':
+                subprocess.run(["git", "pull"])
+                print("Instalando nueva versión...")
 
 check_version()
