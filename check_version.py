67206b21aa554260c1ab076a0a048c4ceb116723
Sergio Morales Álvarez <sergio.morales@>
2023-09-18 11:06:41 +0200

    cambios no operativos

--- check_version.py
+++ check_version.py
@@ -17,16 +17,13 @@ def check():
     # Último release
     latest_release = releases[0]['tag_name']
 
-    if latest_release:
-        # Quito la "v" de las versiones y convierto el número en entero
-        version_number = list(map(int, version.split('v')[-1].split('.')))
-        latest_release_number = list(
-            map(
-                int,
-                latest_release.split('v')[-1].split('.')
-            )
-        )
+    print (latest_release[0]['assets'])
 
-        if version_number < latest_release_number:
-            print("¡¡Hay una nueva realease disponible!!")
-            time.sleep(1)
+    if version < latest_release:
+        mensaje = input ("¡¡Hay una nueva realease disponible!! ¿Quieres actualizar? [S/n]")
+        
+        if mensaje in ['S', 's']:
+            zipball_url = releases[0]['assets']['zipball_url']
+            response = requests.get(zipball_url)
+        elif mensaje in ['N', 'n']:
+            exit()
