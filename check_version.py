9688bcde41dff08ae76102fd50c0325a6a2f0723
Sergio Morales Álvarez <sergio.morales@>
2024-02-26 15:48:38 +0100

    se agrega opción check_update

--- check_version.py
+++ check_version.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python3
 
 import requests
-import time
+import zipfile
+import io
+import configparser
+from settings import config_file
 
+parser = configparser.ConfigParser()
 
 version = 'v0.0.1'
 
 
 def check():
-    # Consulto los realease
-    response = requests.get(
-        "https://api.github.com/repos/sergio1986bcn/awshandler/releases"
-    )
-    releases = response.json()
 
-    # Último release
-    latest_release = releases[0]['tag_name']
+    parser.read(config_file)
 
-    print (latest_release[0]['assets'])
+    if parser['default'].getboolean('check_update', fallback=False):
+        # Consulto los realease
+        response = requests.get(
+            "https://api.github.com/repos/sergio1986bcn/awshandler/releases"
+        )
+        releases = response.json()
 
-    if version < latest_release:
-        mensaje = input ("¡¡Hay una nueva realease disponible!! ¿Quieres actualizar? [S/n]")
-        
-        if mensaje in ['S', 's']:
-            zipball_url = releases[0]['assets']['zipball_url']
-            response = requests.get(zipball_url)
-        elif mensaje in ['N', 'n']:
-            exit()
+        # Último release
+        latest_release = releases[0]['tag_name']
+
+        if version < latest_release:
+            mensaje = input ("¡¡Hay una nueva realease disponible!! ¿Quieres actualizar? [S/n]")
+            
+            if mensaje in ['S', 's']:
+                # Obtiene la url del artefacto
+                zipball_url = releases[0].get("zipball_url")
+                # Descarga el archivo
+                response = requests.get(zipball_url)
+                # Crea un objeto de archivo zip en memoria a partir de los datos descargados
+                zip_data = io.BytesIO(response.content)
+                with zipfile.ZipFile(zip_data, 'r') as zip_ref:
+                    # Especifica la ubicación donde deseas extraer los archivos
+                    extract_path = "" 
+                    zip_ref.extractall(extract_path)
+
+            elif mensaje in ['N', 'n']:
+                exit()
