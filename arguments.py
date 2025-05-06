03202c23ba6f925850b7405d5918ff15d78b7ef1
sergio.morales <sergio.morales@nubersia.com>
2025-05-06 13:46:42 +0000

    subida todo

--- arguments.py
+++ arguments.py
@@ -2,8 +2,10 @@ import argparse
 from check_version import version
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Descripción de tu aplicación.')
+    parser = argparse.ArgumentParser(description='Listado argumentos disponibles.')
     parser.add_argument('-v', '--version', action='version', version=version,
                         help='Muestra la versión del programa y sale.')
+    parser.add_argument('-r', '--repo', action='version', version=version,
+                        help='Crea un nuevo repositorio. Ejemplo: --repo bitbucket nombre-repositorio')
     return parser.parse_args()
     input("Presiona Enter para continuar...")
